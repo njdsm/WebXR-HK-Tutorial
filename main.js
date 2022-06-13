2394cd38731fae3ca1bc780ee034745d5f46d143
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2022-06-13 16:16:54 -0500

    click to add cubes

--- main.js
+++ main.js
@@ -1,51 +1,69 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r132/three.module.js';
 import { ARButton } from 'https://unpkg.com/three@0.132.0/examples/jsm/webxr/ARButton.js';
 
-
 document.addEventListener('DOMContentLoaded', () => {
     const initialize = async () => {
-
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera();
+
+        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        scene.add(light);
+
+        const reticleGeometry = new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI/2);
+        const reticleMaterial = new THREE.MeshBasicMaterial();
+        const reticle = new THREE.Mesh(reticleGeometry, reticleMaterial);
+        reticle.matrixAutoUpdate = false;
+        reticle.visible = false;
+        scene.add(reticle);
 
         const renderer = new THREE.WebGLRenderer({alpha: true});
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.setPixelRatio(window.devicePixelRatio);
         document.body.appendChild(renderer.domElement);
 
-        const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
-        const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(0, 0, -0.3);
-        scene.add(mesh);
-
-        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-        scene.add(light);
-
-        renderer.xr.enabled = true;
-        renderer.setAnimationLoop(() => {
-            renderer.render(scene, camera);
-        });
+        const arButton = ARButton.createButton(renderer, {requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
+        document.body.appendChild(renderer.domElement);
+        document.body.appendChild(arButton);
 
         const controller = renderer.xr.getController(0);
         scene.add(controller);
-
-        const events = document.querySelector("#events");
-
-        
-        controller.addEventListener("select", () => {
-            const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
+        controller.addEventListener('select', () => {
+            const geometry = new THREE.BoxGeometry(0.06,0.06,0.06);
             const material = new THREE.MeshBasicMaterial({color: 0xffffff * Math.random()});
             const mesh = new THREE.Mesh(geometry, material);
-            mesh.position.applyMatrix4(controller.matrixWorld);
-            mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
+
+            mesh.position.setFromMatrixPosition(reticle.matrix);
+            mesh.scale.y = Math.random() * 2 + 1;
             scene.add(mesh);
-            
         });
 
-        const arButton = ARButton.createButton(renderer, {optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
-        document.body.appendChild(renderer.domElement);
-        document.body.appendChild(arButton);
+        renderer.xr.addEventListener("sessionstart", async () => {
+            const session = renderer.xr.getSession();
+
+            const viewerReferenceSpace = await session.requestReferenceSpace("viewer");
+            const hitTestSource = await session.requestHitTestSource({space: viewerReferenceSpace});
+            renderer.setAnimationLoop((timestamp, frame) => {
+                if(!frame) return;
+
+                const hitTestResults = frame.getHitTestResults(hitTestSource);
+                if (getHitTestResults.length > 0){
+                    const hit = hitTestResults[0];
+                    const referenceSpace = renderer.xr.getReferenceSpace();
+                    const hitPose = hit.getPose(referenceSpace);
+                    reticle.visible = true;
+                    reticle.matrix.fromArray(hitPose.transfrom.matrix);
+                } else {
+                    reticle.visible = false;
+                }
+
+                renderer.render(scene, camera);
+            });
+        });
+
+        renderer.xr.addEventListener("sessionstart", async () => {
+            
+        })
+
     }
     initialize();
 })
